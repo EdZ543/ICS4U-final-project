e81e30f841cef732c63b1ad9f7cc3a7f33e56a59
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-01-11 13:41:06 -0500

    Add levels file

---
+++ Levels.java
@@ -0,0 +1,16 @@
+/**
+ * Arrays of all levels in the game
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Levels  
+{
+    public static final String[][] LEVELS = {
+        {"p    "},
+        {"d    "},
+        {" >>v "},
+        {">^dv "},
+        {"ddddd"},
+    };
+}
