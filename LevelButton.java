eefc512df7606ac824cb0dcfcc99398c35899f63
caden <mykkchan@gmail.com>
2023-01-24 23:17:30 -0500

    levelbutton with cool image things

--- LevelButton.java
+++ LevelButton.java
@@ -3,14 +3,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Button for selecting a level to play.
  * 
- * @author Eddie Zhuang
+ * @author Eddie Zhuang, Caden Chan
  * @version Jan. 24, 2023
  */
 public class LevelButton extends UIButton
 {
-    int level;
-    boolean active;
-    
+    private int level;
+    private boolean active;
+    private Label num;
     /**
      * Constructor.
      * 
@@ -22,15 +22,33 @@ public class LevelButton extends UIButton
         super(null);
         this.level = level;
         this.active = active;
-        
-        GreenfootImage image = new GreenfootImage(size, size);
-        image.setColor(Color.BLUE);
-        image.fill();
-        image.setFont(new Font(size));
+        drawImage(active, size);
+    }
+    
+    public void drawImage(boolean active, int size) {
+        GreenfootImage image = new GreenfootImage("level-select.png");
+        image.scale(size, size);
+        image.setFont(new Font("Segoe Print", (int)(size/1.5)));
         image.setColor(Color.WHITE);
-        image.drawString(Integer.toString(level+1), 5, size - 5);
-        if (!active) image.setTransparency(50);
+        
+        // Set text
+        int fontSize = (int)(size/1.5);
+        int textWidth = Label.textPixelWidth(Integer.toString(level+1), "Segoe Print", fontSize);
+        image.drawString(Integer.toString(level+1), size/2 - textWidth/2, fontSize);
+        
+        // Create copy of image for hover
+        GreenfootImage hoverImage = new GreenfootImage(image);
+        hoverImage.scale((int)(image.getWidth()*1.2), (int)(image.getHeight()*1.2));  // hover is bigger than actual image
+        
+        // Make transluscent if inactive
+        if(!active) {
+            image.setTransparency(50);
+            hoverImage.setTransparency(50);
+            setActive(false);
+        }
+        // Set images to superclass
         changeImage(image);
+        changeHoverImage(hoverImage);
     }
     
     /**
@@ -40,12 +58,12 @@ public class LevelButton extends UIButton
         if (active) Greenfoot.setWorld(new LevelWorld(level));
     }
     
-    /**
-     * Sets whether the button is active.
-     * 
-     * @param a Whether the button should be active
-     */
-    public void setActive(boolean a) {
-        active = a;
-    }
+    // /**
+     // * Sets whether the button is active.
+     // * 
+     // * @param a Whether the button should be active
+     // */
+    // public void setActive(boolean a) {
+        // active = a;
+    // }
 }
